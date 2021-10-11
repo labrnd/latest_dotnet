8067bba0eaa52d6d700fd306aa0f1c6b1038d776
labrnd <nextlabdotnet@gmail.com>
2021-10-11 15:42:58 +0300

    Use proper max length for vs code and rider

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
